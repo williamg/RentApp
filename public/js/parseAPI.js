cb257138444535dd1fa0560255d8202f2ec3a97f
deedeethan <deedeethan@users.noreply.github.com>
2015-02-07 02:06:58 -0500

    Added an Item object and User login info

--- public/js/parseAPI.js
+++ public/js/parseAPI.js
@@ -1,15 +1,76 @@
-var parseAPI = {
-	// Creates a new user on the Parse API
-	// user is a javascript object:
-	// user = {name: "John Doe", email: "jdoe@email.com", password: "password"}
-	createUser: function(user) {
-
+var Item = Parse.Object.extend("Item", {
+	initialize: function(attrs, options) {
+		this.name = objName;
+		this.price = objPrice;
+		this.description = objDescription;
+		this.username = user;
+		this.rented = rented;
+	}
+	
+	listItem: function(item) {
+		var item = new Item();
+                item.set("name", name);
+                item.set("price", price);
+                item.set("description", description);
+                item.set("username", user);
+                item.set("rented", rented);
+        	
+        	item.save(null, {
+        		success: function(item) {
+        			// item successfully saved
+        		}
+        		error: function(item, error) {
+        			alert('Failed to add new item, with error code: ' + error.message);
+        		}
+        	});
 	},
+	
+ 	checkoutItem: function(itemID) {
+ 		
+ 	},
+ 	
+ 	checkinItem: function(itemID) {
+ 		
+ 	},
+ 	
+	getItem: function(searchString) {
+			
+	}
 
-	loginUser: function(user) {
-
+	postReview: function(review, itemID) {
+		
 	},
-};
+	
+	getReview: function(itemID) {
+		
+	},
+});
+
+		var user = new Parse.User();
+		user.set("username", user);
+    		user.set("password", password);
+    		user.set("email", email);
+
+    		user.signUp(null, {
+			success: function(user) {
+	        	// After user clicks link to confirm
+	        	// Take user to different page
+        		},
+    			error: function(user, error) {
+        			// Show the error message somewhere and let the user try again.
+        			alert("Error: " + error.code + " " + error.message);
+        		}
+    		}
+	
+		Parse.User.logIn(user, password, {
+			success: function(user) {
+ 			// Do stuff after successful login.
+        		// Take user to different page
+        		},
+    			error: function(user, error) {
+        			// The login failed. Check error to see why.
+        		}
+		}
 
 function testObject() {
 	var TestObject = Parse.Object.extend("TestObject");
@@ -18,5 +79,12 @@ function testObject() {
 	});
 }
 
+function testUser() {
+	var user = new User();
+	user.save({user: "student"}).then(function(object) {
+	});
+}
+
 Parse.initialize("Tn3zdIeNZveir6giGyxWoMmW4PK2KGOk1tsZYg5D", "SEOr0YmRGbxa4dTqn5ifEXn5my5Uz2KWAJ2J9wG2");
 testObject();
+testUser();
